6138d3e25466c6442691a28d07ee7446c7db0e99
NomuKen13 <110008733+NomuKen13@users.noreply.github.com>
2022-11-04 21:08:44 +0900

    目標ペースからBPMを計算する機能を仮設しました

--- lib/BPMselectpage.dart
+++ lib/BPMselectpage.dart
@@ -3,6 +3,7 @@ import 'package:beatim/musicselectfunction.dart';
 import 'package:beatim/playpage.dart';
 import 'package:flutter/material.dart';
 import 'variables.dart';
+import 'dart:math';
 
 class BPMSelectPage extends StatefulWidget {
   const BPMSelectPage({Key? key}) : super(key: key);
@@ -13,10 +14,15 @@ class BPMSelectPage extends StatefulWidget {
 
 class _BPMSelectPageState extends State<BPMSelectPage> {
   @override
-  double _ratio = 1;
-  double _min_ratio = 0.7;
-  double _max_ratio = 1.5;
+
+  //ペース調整バーに使う変数。それぞれ　バーの初期値、バーの最小値、バーの最大値
+  double _runningpase = 333;
+  double _min_runningpase = 240;
+  double _max_runningpase = 420;
+
+  //BPM算出に使う。BPM＝(この値)/走るペース
   double _pase = 333*176;
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -26,60 +32,98 @@ class _BPMSelectPageState extends State<BPMSelectPage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            TextButton(onPressed: (){
-              Navigator.push(context,MaterialPageRoute(builder:(context) =>(PlayPage())));
-              setState(() {
-                sensingBPM = previous_sensingBPM - 10;
-                previous_sensingBPM -= 10;
-              });
-              playlist = musicselect(genre:genre, artist:artist, BPM:sensingBPM);
-            }, child: Text("前回より遅め(${previous_sensingBPM - 10})(${((_pase/_ratio)/(previous_sensingBPM-10))~/60}min${(((_pase/_ratio)/(previous_sensingBPM-10))%60).toInt()}s/km)")
+
+            //ペースから計算する部分
+            Container(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("ペースから自動で計算", style: TextStyle(fontSize: 20),),
+                  SizedBox(height: 10,),
+                  Text("${_runningpase~/60}min${(_runningpase%60).toInt()}s/km", style: TextStyle(fontSize: 30),),
+                  Text("BPM:${(176*333/_runningpase).toStringAsFixed(1)}",style: TextStyle(fontSize: 30),),
+                  Text("ペースを設定",style: TextStyle(fontSize: 20),),
+                  Transform.rotate(
+                    angle: pi,
+                    child: Slider(
+                        inactiveColor: Colors.blue,
+                        activeColor: Colors.blue.shade50,
+                        thumbColor: Colors.blue,
+                        value: _runningpase,
+                        min: _min_runningpase,
+                        max: _max_runningpase,
+                        onChanged:(value){
+                          setState(() {
+                            _runningpase = value;
+                          });
+                        }
+                    ),
+                  ),
+                  ElevatedButton(onPressed: (){
+                    Navigator.push(context,MaterialPageRoute(builder:(context) =>PlayPage()));
+                    setState(() {
+                      sensingBPM = 176*333/_runningpase;
+                      previous_sensingBPM = 176*333/_runningpase;
+                    });
+                    playlist = musicselect(genre:genre, artist: artist, BPM: sensingBPM);
+                  }, child: Text("このペースで走る",style: TextStyle(fontSize: 30),)
+                  ),
+                ],
+              ),
             ),
-            TextButton(onPressed: (){
-              Navigator.push(context,MaterialPageRoute(builder:(context) =>PlayPage()));
-              setState(() {
-                sensingBPM = previous_sensingBPM;
-              });
-              playlist = musicselect(genre:genre, artist:artist, BPM:sensingBPM);
-            }, child: Text("前回(${previous_sensingBPM})(${((_pase/_ratio)/previous_sensingBPM)~/60}min${(((_pase/_ratio)/previous_sensingBPM)%60).toInt()}s/km)")
+            //中間にある隙間。
+            SizedBox(height: 50,),
+            Container(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("前回のBPMで再開", style: TextStyle(fontSize: 20),),
+                  SizedBox(height: 10,),
+                  TextButton(onPressed: (){
+                    Navigator.push(context,MaterialPageRoute(builder:(context) =>(PlayPage())));
+                    setState(() {
+                      sensingBPM = previous_sensingBPM - 10;
+                      previous_sensingBPM -= 10;
+                    });
+                    playlist = musicselect(genre:genre, artist:artist, BPM:sensingBPM);
+                  }, child: Text("前回より遅め(${previous_sensingBPM - 10})(${((_pase)/(previous_sensingBPM-10))~/60}min${(((_pase)/(previous_sensingBPM-10))%60).toInt()}s/km)")
+                  ),
+                  TextButton(onPressed: (){
+                    Navigator.push(context,MaterialPageRoute(builder:(context) =>PlayPage()));
+                    setState(() {
+                      sensingBPM = previous_sensingBPM;
+                    });
+                    playlist = musicselect(genre:genre, artist:artist, BPM:sensingBPM);
+                  }, child: Text("前回(${previous_sensingBPM})(${((_pase)/previous_sensingBPM)~/60}min${(((_pase)/previous_sensingBPM)%60).toInt()}s/km)")
+                  ),
+                  TextButton(onPressed: (){
+                    Navigator.push(context,MaterialPageRoute(builder:(context) =>PlayPage()));
+                    setState(() {
+                      sensingBPM = previous_sensingBPM + 10;
+                      previous_sensingBPM += 10;
+                    });
+                    playlist = musicselect(genre:genre, artist: artist, BPM: sensingBPM);
+                  }, child: Text("前回より早め(${previous_sensingBPM + 10})(${((_pase)/(previous_sensingBPM+10))~/60}min${(((_pase)/(previous_sensingBPM+10))%60).toInt()}s/km)")
+                  ),
+                ],
+              ),
             ),
-            TextButton(onPressed: (){
-              Navigator.push(context,MaterialPageRoute(builder:(context) =>PlayPage()));
-              setState(() {
-                sensingBPM = previous_sensingBPM + 10;
-                previous_sensingBPM += 10;
-              });
-              playlist = musicselect(genre:genre, artist: artist, BPM: sensingBPM);
-            }, child: Text("前回より早め(${previous_sensingBPM + 10})(${((_pase/_ratio)/(previous_sensingBPM+10))~/60}min${(((_pase/_ratio)/(previous_sensingBPM+10))%60).toInt()}s/km)")
-            ),
-            TextButton(onPressed: () async {
-              setState(() {
-                comefrom = "bpmselectpage";
-              });
-              // player.pause();
-              await Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder:(context) => BPMSensingPage()
-                  )
-              );
-              setState(() {});
-            }, child: Text("新しく登録（計測ページへ）")),
-            TextButton(onPressed: (){
-              Navigator.pop(context);
-            }, child: Text("アーティスト選択にもどる")
-            ),
-            SizedBox(width: 0,height: 20,),
-            Text("走るペース"),
-            Slider(
-                value: _ratio,
-                min: _min_ratio,
-                max: _max_ratio,
-                onChanged:(value){
-                  setState(() {
-                    _ratio = value;
-                  });
-                }
-                )
+            // TextButton(onPressed: () async {
+            //   setState(() {
+            //     comefrom = "bpmselectpage";
+            //   });
+            //   // player.pause();
+            //   await Navigator.of(context).push(
+            //     MaterialPageRoute(
+            //       builder:(context) => BPMSensingPage()
+            //       )
+            //   );
+            //   setState(() {});
+            // }, child: Text("新しく登録（計測ページへ）")),
+            // TextButton(onPressed: (){
+            //   Navigator.pop(context);
+            // }, child: Text("アーティスト選択にもどる")
+            // ),
           ],
         ),
       ),
--- lib/variables.dart
+++ lib/variables.dart
@@ -1,18 +1,18 @@
 import 'package:flutter/material.dart';
 
-var genre = "J-POP";
-var artist = "ミセス";
-double sensingBPM = 150;
-double previous_sensingBPM = 165;
-var playlist = [0,0,0,0,0];
-var music = "audio/Mrs. GREEN APPLE_青と夏__BPM185.mp3";
-var playericon = Icons.play_arrow;
-var visible = false;
-var changingspeed = true;
-var changingspeedbutton = "原曲";
-String comefrom = "bpmselectpage";
+var genre = "J-POP"; //ジャンル
+var artist = "ミセス"; //アーティスト
+double sensingBPM = 150; //計測したBPM。再生速度はこれに合わせる。
+double previous_sensingBPM = 165; //前回再生したBPM。BPM選択ページで使う。
+var playlist = [0,0,0,0,0]; //プレイリスト。ここには曲のIDが入る。playpageではこの順に曲が表示・再生される。
+var music = "audio/Mrs. GREEN APPLE_青と夏__BPM185.mp3"; //再生する曲のファイル名。
+var playericon = Icons.play_arrow; //playpageの下の方に表示されるマーク play_arrow:再生マーク　pause:停止マーク
+var visible = false; //「評価ページへ」ボタンと「再生中の曲を表示するやつ」が表示されるかどうか。一曲目がされるまでfalseにするのが望ましい。
+var changingspeed = true; //曲の再生速度を変えているかどうか。「原曲」「走速」の表示切り替えに使う。
+var changingspeedbutton = "原曲"; //「再生中の曲を表示するやつ」の原曲走速を切り替えるボタンに表示される文字。
+String comefrom = "bpmselectpage"; //BPMsensingpage　に遷移した際BPMselectpageとplaypageどちらからきたのかを示す。
 
-double bpm_ratio = 1.0;
+double bpm_ratio = 1.0; //BPM比であり曲の再生速度
 double ORIGINAL_musicBPM = 138; //goodnight:138, bgm1:152
 bool _changeAudioSource = true;
 String _stateSource = 'アセットを再生';
