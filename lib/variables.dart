60aa16e5c4b1875e6b1e008496f35aef4687ed80
NomuKen13 <110008733+NomuKen13@users.noreply.github.com>
2023-01-12 17:52:29 +0900

    不要なコードの削除

--- lib/variables.dart
+++ lib/variables.dart
@@ -1,11 +1,7 @@
-import 'package:just_audio/just_audio.dart';
-import 'musicdata.dart';
-
 var genre = "J-POP"; //ジャンル
 var artist = "ミセス"; //アーティスト
 double sensingBPM = 165; //計測したBPM。再生速度はこれに合わせる。
-var playlist = [0, 0, 0, 0, 0]; //プレイリスト。ここには曲のIDが入る。playpageではこの順に曲が表示・再生される。
-var music = "audio/Mrs. GREEN APPLE_青と夏__BPM185.mp3"; //再生する曲のファイル名。
+var playlist = [0]; //プレイリスト。ここには曲のIDが入る。playpageではこの順に曲が表示・再生される。
 var visible =
     false; //「評価ページへ」ボタンと「再生中の曲を表示するやつ」が表示されるかどうか。一曲目がされるまでfalseにするのが望ましい。
 String link = "";
@@ -13,15 +9,7 @@ String musicname = "";
 int numberofmusics = 0;
 int? previousIndex = -1;
 
-double bpmRatio = 1.0; //BPM比であり曲の再生速度
-double originalMusicBPM = 138; //goodnight:138, bgm1:152
 int oldtime = 0;
 int newtime = 0;
 List<int> duls = [1, 1, 1, 1, 1, 1, 1];
 
-ConcatenatingAudioSource newplaylist = ConcatenatingAudioSource(
-  children: List.generate(
-      playlist.length,
-      (inde) => AudioSource.uri(
-          Uri.parse('asset:${musics[playlist[inde]]['filename']}'))),
-);
